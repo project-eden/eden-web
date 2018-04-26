23596e0af48510f6e5c218e0f70cb5f4fed64b78
Emilio Aurea <emilioaurea@berkeley.edu>
2018-04-25 21:09:58 -0700

    React deployed to firebase

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import * as firebase from 'firebase';
-
-  var config = {
-    apiKey: "AIzaSyBnMlXKxQmneHPgG-u5JP9wu1z45DDu75s",
-    authDomain: "project-eden-bf70e.firebaseapp.com",
-    databaseURL: "https://project-eden-bf70e.firebaseio.com",
-    projectId: "project-eden-bf70e",
-    storageBucket: "project-eden-bf70e.appspot.com",
-    messagingSenderId: "656452826349"
-  };
-  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
