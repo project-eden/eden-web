f21b2faf8dca8bab60a80a5c9aefb5e3cfdc8bb4
Christian <chnicoloso@gmail.com>
2018-04-30 22:29:37 -0700

    add a dynamic heatmap

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper, HeatMap} from 'google-maps-react';
 import PropTypes from 'prop-types';
 import config from 'constants/config.json';
+import { pixel_resolutions } from 'constants/geoMappings.json';
 import { getTopNCrops, whichCountry, allCoordinates } from 'utils/request';
 import InfoCard from './InfoCard/InfoCard';
 import SidePanel from './SidePanel/SidePanel';
@@ -24,73 +25,35 @@ export class MapContainer extends Component {
     map.setMapTypeId('satellite');
     map.setTilt(45);
     map.set('streetViewControl', false);
-    map.set('minZoom', 2);
-    map.set('maxZoom', 13);
+    map.set('minZoom', 3);
+    map.set('maxZoom', 12);
     map.set('zoom', 3);
     map.setCenter({lat:20, lng:-20});
 
     this.createHeatMap(map);
   }
 
-  calcBounds(center,size){
-
-    const n = this.props.google.maps.geometry.spherical.computeOffset(center,size.height /2,0).lat(),
-      s = this.props.google.maps.geometry.spherical.computeOffset(center,size.height / 2,180).lat(),
-      e = this.props.google.maps.geometry.spherical.computeOffset(center,size.width / 2,90).lng(),
-      w=this.props.google.maps.geometry.spherical.computeOffset(center,size.width /2,270).lng();
-
-      return  new this.props.google.maps.LatLngBounds(new this.props.google.maps.LatLng(s,w), new this.props.google.maps.LatLng(n,e));
-}
+  getRadius(zoomLevel) {
+    // 10km * the pixel resolution for the given zoom level.
+    return 10000 / pixel_resolutions[String(zoomLevel)];
+  }
 
   createHeatMap(map) {
     allCoordinates().then(data => {
-      console.log('finished');
       let heatmapdata = [];
 
+      // TODO Remove this map randomizer.
       data.sort( function() { return 0.5 - Math.random() } );
-
       var startingPoint = Math.floor(Math.random()*(data.length-0+1)+0);
-      // data.slice(startingPoint, startingPoint + 2000).forEach(location => {
-      //   // new this.props.google.maps.Rectangle({bounds:this.calcBounds(new this.props.google.maps.LatLng(location[1], location[0]), new this.props.google.maps.Size(10000,10000)),map:map});
-      //   var cityCircle = new this.props.google.maps.Circle({
-      //     strokeColor: '#FF0000',
-      //     strokeOpacity: 0.35,
-      //     strokeWeight: 2,
-      //     fillColor: '#FF0000',
-      //     fillOpacity: 0.35,
-      //     map: map,
-      //     center: new this.props.google.maps.LatLng(location[1], location[0]),
-      //     radius: 2800,
-      //     clickable: false
-      //   });
-      // });
 
-      // var square = new this.props.google.maps.Polygon(berkeley, 50000, 10000, 50000, 10000, -60)
-
-      var gradient = [
-        'rgba(255, 0, 0, 0)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)',
-        'rgba(255, 0, 0, 1)'
-      ];
-
-      data.slice(startingPoint, startingPoint+10000).forEach(location => {
+      data.slice(startingPoint, startingPoint+2000).forEach(location => {
         heatmapdata.push(new this.props.google.maps.LatLng(location[1], location[0]));
       });
 
       var heatmap = new this.props.google.maps.visualization.HeatmapLayer({
         data: heatmapdata,
         gradient: gradient,
+        opacity: 0.7
       });
 
       heatmap.setMap(map);
@@ -98,40 +61,15 @@ export class MapContainer extends Component {
         heatmap
       });
 
-      // map.addListener('zoom_changed', () => {
-      //   let radius;
-      //
-      //   switch (map.getZoom()) {
-      //     case 3:
-      //       radius = 15
-      //       break;
-      //     case 4:
-      //       radius = 15
-      //       break;
-      //     case 5:
-      //       radius = 25
-      //       break;
-      //     case 6:
-      //       radius = 45
-      //       break;
-      //     case 7:
-      //       radius = 95
-      //       break;
-      //     case 8:
-      //       radius = 180
-      //       break;
-      //     case 9:
-      //       radius = 400
-      //       break;
-      //     default:
-      //       radius = undefined;
-      //   }
-      //   if (radius) {
-      //     this.state.heatmap.setOptions({
-      //       radius: radius
-      //     });
-      //   }
-      // });
+      // Dynamically set the radius of the heat map points to preserve resolution.
+      map.addListener('zoom_changed', () => {
+        const zoomLevel = map.getZoom();
+        if (zoomLevel >= 8) {
+          this.state.heatmap.setOptions({
+            radius: this.getRadius(zoomLevel)
+          });
+        }
+      });
     });
   }
 
@@ -146,8 +84,6 @@ export class MapContainer extends Component {
         sidePanelOpen: true
       });
     });
-    console.log(map.getZoom());
-    // console.log(this.state.heatmap.get('radius'));
   }
 
   render() {
